53801b83e2a66b6a25f76c2a87c0ac0367044e52
John Baxter <jbaxter@a-zdarts.com>
2021-01-16 17:10:52 -0800

    built out base template, added twilio, setup inquirer, can send a text message successfully

---
+++ index.js
@@ -0,0 +1,28 @@
+// =========== Const Section ===========
+const INQUIRER = require("inquirer");
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+// ========== END Const ==================
+
+// array of questions for user
+console.log('meow');
+
+// function to initialize program
+function init() {
+  INQUIRER.prompt([{type: 'input',message:'Enter Phone Number: ',name:'number'},{type: 'input',message:'Enter Message: ',name:'message'}])
+    .then((answers) => {
+        console.log(answers);
+    client.messages
+      .create({
+        body: answers.message,
+        from: "+17632805723",
+        to: `+${answers.number}`,
+      })
+      .then((message) => console.log(message));
+  });
+}
+
+// function call to initialize program
+init();
